23cdb77fc2f005f308213f243d9782dde8c98f9b
manuja <rpmanuja123@gmail.com>
2025-05-24 13:06:55 +0530

    x

--- react-5/src/App.jsx
+++ react-5/src/App.jsx
@@ -6,29 +6,12 @@ import viteLogo from '/vite.svg'
 function App() {
   
 
-  function hello1(){
-    console.log("hello 1");
-  }
-
-   const hello2= () =>{
-    console.log("hello 2");
-  }
-
-  function hello3(name){
-    console.log("hello3"+ name);
-  }
-
-  const hello4 = (name) => {
-    console.log("hello4" + name);
-  }
 
 
   return (
     <>
       <div id="wrapper">
-        <button onClick={hello1}>click 1</button>
-        <button onClick={hello2}>click 2</button>
-        <button onClick={() =>{hello3("manuja")}}>click 2</button>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi error fuga omnis voluptatem possimus architecto ab temporibus adipisci laboriosam ipsum repellat, odio iure doloremque itaque sapiente officiis obcaecati. Reprehenderit, necessitatibus.</p>
         
         
       </div>
