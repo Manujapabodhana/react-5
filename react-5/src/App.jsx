f47ea3992b0ff984e60d150ceb9cd7c1d728bc95
manuja <rpmanuja123@gmail.com>
2025-05-24 11:47:47 +0530

    n

--- react-5/src/App.jsx
+++ react-5/src/App.jsx
@@ -14,11 +14,23 @@ function App() {
     console.log("hello 2");
   }
 
+  function hello3(name){
+    console.log("hello3"+ name);
+  }
+
+  const hello4 = (name) => {
+    console.log("hello4" + name);
+  }
+
+
   return (
     <>
       <div id="wrapper">
         <button onClick={hello1}>click 1</button>
         <button onClick={hello2}>click 2</button>
+        <button onClick={() =>{hello3("manuja")}}>click 2</button>
+        
+        
       </div>
     
     </>
